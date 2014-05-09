c992f855186db3eecc7ce82425b314b60beafbb7
Kasper Balink <kasperbalink@hotmail.com>
2014-05-09 14:39:46 +0200

    asdfasdf

--- src/model/Player.java
+++ src/model/Player.java
@@ -7,12 +7,21 @@ public class Player {
 	private int score;
 	private int bullets;
 	private int amountIR;
+	private double shots;
+	private double hits;
+	private double accuracy;
 	private IRSource[] irsource;
 	
 	public Player()
 	{
 		this.score = 0;
 		this.bullets = 25;
+		this.setShots(0);
+	}
+	
+	public void update()
+	{
+
 	}
 	public int getScore() {
 		return score;
@@ -21,6 +30,12 @@ public class Player {
 	public void shot()
 	{
 		bullets--;
+		setShots(getShots() + 1);
+	}
+	
+	public void hit()
+	{
+		setHit(getHit() + 1);
 	}
 	public void setScore(int score) {
 		this.score = score;
@@ -42,6 +57,27 @@ public class Player {
 	}
 	public void setIrsource(IRSource[] irsource) {
 		this.irsource = irsource;
+	}
+	public int getShots() {
+		return (int)shots;
+	}
+	public void setShots(int shots) {
+		this.shots = shots;
+	}
+	public int getHit() {
+		return (int)hits;
+	}
+	public void setHit(int hit) {
+		this.hits = hit;
+	}
+	public double getAccuracy() 
+	{
+		accuracy = (hits / shots);
+		System.out.println("Accuracy: -----------------------------------" + accuracy);
+		return accuracy;
+	}
+	public void setAccuracy(int accuracy) {
+		this.accuracy = accuracy;
 	}
 	
 }
